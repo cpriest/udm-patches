5c8049907799f50b5b2638b8b839add4247f81bb
Clint Priest <cpriest@resultsgeneration.com>
2021-06-27 15:25:43 -0500

    Added better logging to /var/log/openvpn-orig.log

--- udm-patches/scripts/openvpn-wrapper.sh
+++ udm-patches/scripts/openvpn-wrapper.sh
@@ -6,6 +6,14 @@ PROFILE="$(echo \"$args\" | grep -oE 'openvpn-[0-9]+')";
 
 UDM_DIR="/mnt/data/udm-patches"
 PROF_DIR="${UDM_DIR}/${PROFILE}/"
+LOG_FILE="/tmp/openvpn-orig.log";
+
+{
+	echo "********* OpenVPN Wrapper Intercept *********";
+	echo "UDM Called: $0 $*";
+	echo "   PROFILE_NUM=${PROFILE}, PROF_DIR=${PROF_DIR}"; 
+	echo "********* OpenVPN Wrapper Intercept *********";
+} >> "$LOG_FILE";
 
 # If there is an openvpn-N profile directory, use that configuration instead
 if [[ -e "$PROF_DIR" ]]; then
@@ -13,7 +21,7 @@ if [[ -e "$PROF_DIR" ]]; then
 	mv /run/${PROFILE}/* /run/${PROFILE}/.disabled;
 	cp ${PROF_DIR}/*.conf /run/${PROFILE}/${PROFILE}.conf;
 
-	exec /usr/sbin/openvpn-orig --config /run/${PROFILE}/${PROFILE}.conf >& /tmp/openvpn-orig.log
+	exec /usr/sbin/openvpn-orig --config /run/${PROFILE}/${PROFILE}.conf 2>&1 >> "${LOG_FILE}";
 else
 	exec /usr/sbin/openvpn-orig $*
 fi;
